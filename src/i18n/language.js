5b56965555ab6773b22e81a2905da18b1d58888c
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-01-24 15:24:23 +0800

    Update language.js

--- src/i18n/language.js
+++ src/i18n/language.js
@@ -3,6 +3,11 @@ const messages = {
         message:{
             gamemode:'Statistical game mode by:{id}'
         }
+    },
+    zh:{
+        message:{
+            gamemode:'统计游戏模式 by:{id}'
+        }
     }
   }
   
