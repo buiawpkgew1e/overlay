f1e6795387e2b99e52d1ec4c7d59484f2e814fec
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-01-24 17:40:57 +0800

    w

---
+++ src/i18n/language.js
@@ -0,0 +1,18 @@
+const messages = {
+    en: {
+        message:{
+            gamemode:'Statistical game mode by:{id}',
+            bw:'Bedwars',
+            sw:'Skywars',
+            duels:'duels'
+        }
+    },
+    zh:{
+        message:{
+            gamemode:'统计游戏模式 by:{id}'
+        }
+    },
+    ja:{
+
+    }
+  }
